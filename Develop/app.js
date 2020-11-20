498e9854f05a97eab2e3085baa0b6b88100a0f86
Dustin Pidcock <dustygen@gmail.com>
2020-11-19 18:42:11 -0800

    npm giving me a hard time

--- Develop/app.js
+++ Develop/app.js
@@ -1,7 +1,8 @@
+const inquirer = require("inquirer");
+
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
@@ -10,6 +11,33 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+function runInquire() {
+  const promptArray = [
+    {
+      type: "input",
+      message: "What's your name?",
+      name: "name",
+    },
+    {
+      type: "input",
+      message: "What's your Id?",
+      name: "id",
+    },
+    {
+      type: "input",
+      message: "What's your email?",
+      name: "email",
+    },
+    {
+      type: "list",
+      message: "What's your title?",
+      choices: ["Manager", "Engineer", "Intern"],
+      name: "title",
+    },
+  ];
+  return inquirer
+        .prompt(promptArray);
+}
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
